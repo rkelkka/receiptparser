7f1e1048bc1a3b91517979585f43c193d7b0e9a5
rkelkka <raine_kelkka@hotmail.com>
2016-11-05 16:23:21 +0200

    Rename template class1 to PdfParser.

--- src/ReceiptParser/PdfParser.cs
+++ src/ReceiptParser/PdfParser.cs
@@ -3,12 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace receiptparser
+namespace ReceiptParser
 {
-    public class Class1
+    public class PdfParser
     {
-        public Class1()
-        {
-        }
     }
 }
